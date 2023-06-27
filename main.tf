a3dfb282eb31b512654ec7278fa6ea3a3857df2d
jhs512 <jangka512@gmail.com>
2023-06-27 11:12:12 +0900

    서브넷 추가

--- main.tf
+++ main.tf
@@ -32,3 +32,11 @@ resource "aws_vpc" "example" {
   }
 }
 
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "example_subnet"
+  }
+}
