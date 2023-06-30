26ff39d154745dd13f48704592d65b93837a102d
jhs512 <jangka512@gmail.com>
2023-06-30 11:40:00 +0900

    vpc 에서 dns 허용

--- main.tf
+++ main.tf
@@ -27,6 +27,9 @@ provider "aws" {
 resource "aws_vpc" "vpc_1" {
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "${var.prefix}-vpc-1"
   }
